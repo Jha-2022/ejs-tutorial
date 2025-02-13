1a2e1d7fb68c40f8e3a2ad354b8b43ce7045c55a
Jha-2022 <rishijha0910@gmail.com>
2025-02-13 13:26:13 +0530

    commited

---
+++ car.js
@@ -0,0 +1,9 @@
+const car = [
+    {name:"Audi R8", year:2008},
+    {name:"mutang GT", year: 1969},
+    {name:"amg",year:2014}
+]
+
+for(let i=0; i<car.length; i++){
+    console.log("the " +car[i].name +" was launched in the year "+ car[i].year )
+}
